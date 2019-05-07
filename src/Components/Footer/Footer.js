e45fd5c319975e32bfd85de2137b5363d6c0a376
Austin Burgard <awburgard@gmail.com>
2019-05-07 17:56:29 -0500

    added and linked all files

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <p>© React Actors</p>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
