47845334f35f95aa6cb1b0fbc51952608c452093
Austin Burgard <awburgard@gmail.com>
2019-05-07 18:52:03 -0500

    i think I completed the base goals?

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,12 +6,34 @@ import Contact from '../Contact/Contact';
 import Footer from '../Footer/Footer';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      bestImpression: ''
+    }
+
+  }
   render() {
     return (
       <div className="App">
         <Header />
         <About />
-        <Contact does this work?/>
+        <Contact />
+        <div>
+        <input type="text"
+          data-type="firstName"
+          placeholder="Last Name" />
+        <input type="text"
+          data-type="lastName"
+          placeholder="Last Name" />
+        <input type="text"
+          data-type="bestImpression"
+          placeholder="Best Impression" />
+          <button>Join Us!</button>
+          </div>
         <Footer />
       </div>
     );
