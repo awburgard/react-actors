0f64de78033c44f5e6197b0b82c24f9e9812ef72
Austin Burgard <awburgard@gmail.com>
2019-05-07 17:57:49 -0500

    initial commit

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <About />
-        <Contact />
+        <Contact does this work?/>
         <Footer />
       </div>
     );
